d8caa0776337fdf5f509e354224ae2cced2c0cbc
leonelaadalid <63307794+leonelaadalid@users.noreply.github.com>
2020-12-10 09:57:23 -0300

    Add files via upload

---
+++ Grid-main/idex.js
@@ -0,0 +1,20 @@
+const Capacidad = [{
+    Capacidad : "Dos personas" ,
+    Superficie : "45 m²" ,
+    Balcón : "SI" ,
+    Caracteritica: "Vistas al lago Nahuel Huapi",
+},
+{
+    Capacidad : "Una persona" ,
+    Superficie : "24 m²" ,
+    Balcón : "SI" ,
+    Caracteritica: "vistas al lago Nahuel Huapi",
+},
+{
+    Capacidad : "Dos adultos , un niño" ,
+    Superficie : "48 m²" ,
+    Balcón : "NO" ,
+    Caracteritica: "Estudio con vistas al jardín",
+},
+
+];
